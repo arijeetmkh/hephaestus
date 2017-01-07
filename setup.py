9a90841999a4eda0dcba8e44b31c32e18fd2be09
Arijeet Mukherjee <arijeet@zopper.com>
2017-01-08 01:40:04 +0530

    name lower case, add url

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 
 setup(
-    name='Hephaestus',
+    name='hephaestus',
     version='0.1.0',
     description='Queue Consumer',
     author='Arijeet Mukherjee',
     author_email='arijeet.mkh@gmail.com',
+    url="https://github.com/arijeetmkh/hephaestus",
     packages=['hephaestus'],
     requires=['boto3'],
     scripts=['hephaestus/startup.py']
