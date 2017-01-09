f4242114ee0f18e6fc7609d0f186315a10f45139
Arijeet Mukherjee <arijeet@zopper.com>
2017-01-09 20:58:52 +0530

    fix package data errors

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ setup(
     packages=['hephaestus'],
     install_requires=['boto3'],
     scripts=['bin/hephaestus'],
-    include_package_data=True,
+    package_data={
+        'hephaestus': ['hephaestus.conf', 'message_transport_conf.json']
+    },
     zip_safe=False
 )
