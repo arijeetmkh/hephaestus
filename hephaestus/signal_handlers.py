09a4cddea064ec890cdc04aa8b56277925a81e10
Arijeet Mukherjee <arijeet@zopper.com>
2018-04-23 17:55:25 +0530

    fix singal handler logging + safe shutdowns

--- hephaestus/signal_handlers.py
+++ hephaestus/signal_handlers.py
@@ -2,15 +2,17 @@ import signal
 import logging
 from hephaestus import worker
 
-signalhandlerLogger = logging.getLogger('hephaestus.startup')
 
 signal_name_id_mapping = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
      if v.startswith('SIG') and not v.startswith('SIG_'))
 
 
 def signal_handler(sig, frame):
+    signalhandlerLogger = logging.getLogger('hephaestus.startup')
     signalhandlerLogger.info('%s Signal Received' % signal_name_id_mapping[sig])
     worker.clean_shutdown()
 
+
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGHUP, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
--- hephaestus/worker.py
+++ hephaestus/worker.py
@@ -15,7 +15,8 @@ except ImportError:
 
 workerLogger = logging.getLogger('hephaestus.worker')
 messageReceiverLogger = logging.getLogger('hephaestus.message_receiver')
-_shutdownEvent = threading.Event()
+_SQSWorkerShutdownEvent = threading.Event()
+_MessageWorkerShutdownEvent = threading.Event()
 
 
 class SQSWorker(threading.Thread):
@@ -41,7 +42,7 @@ class SQSWorker(threading.Thread):
         queue = sqs.get_queue_by_name(QueueName=settings.SQS_QUEUE_NAME)
         workerLogger.info("SQS Queue- %s" % str(queue))
         receive_params = self.init_receive_params()
-        while not _shutdownEvent.is_set():
+        while not _SQSWorkerShutdownEvent.is_set():
             workerLogger.debug("Waiting between SQS requests for %d seconds" % settings.SQS_WAIT_BETWEEN_REQUESTS)
             time.sleep(settings.SQS_WAIT_BETWEEN_REQUESTS)
             workerLogger.debug("Connecting to SQS to receive messages with params %s" % str(receive_params))
@@ -56,7 +57,7 @@ class SQSWorker(threading.Thread):
                 workerLogger.exception("Connection to queue failed. Retrying in %d seconds. Original exception: " % settings.RECONNECT_WAIT_TIME)
                 time.sleep(settings.RECONNECT_WAIT_TIME)
 
-        if _shutdownEvent.is_set():
+        if _SQSWorkerShutdownEvent.is_set():
             workerLogger.info('Exiting...')
 
 
@@ -68,7 +69,7 @@ class MessageWorker(threading.Thread):
         workerLogger.info('Message worker started - %s' % str(self.name))
 
     def run(self):
-        while not _shutdownEvent.is_set():
+        while not _MessageWorkerShutdownEvent.is_set():
             try:
                 message = self.messageQueue.get(True, settings.MESSAGE_QUEUE_WAIT_TIMEOUT)
             except queue.Empty:
@@ -89,34 +90,55 @@ class MessageWorker(threading.Thread):
                     message.delete()
                     workerLogger.debug("Message deleted according to policy '%s'" % str(settings.SQS_MESSAGE_DELETE_POLICY))
 
-        if _shutdownEvent.is_set():
+        if _MessageWorkerShutdownEvent.is_set():
             workerLogger.info('Exiting...')
 
 
+_SQSWorkers, _MessageWorkers = [], []
+
+
 def start_workers(transport=None):
-    _shutdownEvent.clear()
+    _SQSWorkerShutdownEvent.clear()
+    _MessageWorkerShutdownEvent.clear()
     workerLogger.debug('Initializing internal message queue with size - %d' % settings.MESSAGE_QUEUE_MAX_SIZE)
     messageQueue = queue.Queue(maxsize=settings.MESSAGE_QUEUE_MAX_SIZE)
 
     workerLogger.info('Spawning %d queue workers' % settings.QUEUE_WORKERS)
     for i in range(settings.QUEUE_WORKERS):
-        SQSWorker(
+        t = SQSWorker(
             messageQueue=messageQueue,
             name="SQSWorker-{worker_number}".format(worker_number=str(i))
-        ).start()
+        )
+        _SQSWorkers.append(t)
+        t.start()
 
     workerLogger.info('Spawning %d message processor workers' % settings.MESSAGE_PROCESSOR_WORKERS)
     for i in range(settings.MESSAGE_PROCESSOR_WORKERS):
-        MessageWorker(
+        t = MessageWorker(
             messageQueue=messageQueue,
             transport=transport,
             name="MessageWorker-{worker_number}".format(worker_number=str(i))
-        ).start()
+        )
+        _MessageWorkers.append(t)
+        t.start()
 
 
 def clean_shutdown():
-    if _shutdownEvent.is_set():
+    if _SQSWorkerShutdownEvent.is_set() or _MessageWorkerShutdownEvent.is_set():
         workerLogger.warning('Clean shutdown already initiated. ** Cold shutdown not yet implemented **')
     else:
         workerLogger.info('Received Interrupt. Starting clean shutdown')
-    _shutdownEvent.set()
+
+    workerLogger.debug("Sending shutdown event to SQS Workers")
+    _SQSWorkerShutdownEvent.set()
+    [_.join() for _ in _SQSWorkers]
+
+    workerLogger.info("All SQS Workers exited")
+
+    while not _MessageWorkers[0].messageQueue.empty():
+        workerLogger.debug("Local message queue not empty. Waiting...")
+        time.sleep(0.5)
+
+    if _MessageWorkers:
+        workerLogger.debug("Sending shutdown event to Message Workers")
+        _MessageWorkerShutdownEvent.set()
